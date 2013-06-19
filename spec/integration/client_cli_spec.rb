151d0e7f3c3fac403a198447cf4528975b1d19b1
Justin Force <justin.force@gmail.com>
2013-06-18 18:11:34 -0700

    Wait for server to start to run CLI integration spec

--- spec/integration/client_cli_spec.rb
+++ spec/integration/client_cli_spec.rb
@@ -2,12 +2,28 @@ require 'spec_helper'
 require 'signet/configuration'
 require 'signet/http_server'
 require 'support/openssl_helpers'
+require 'timeout'
 
 include Signet::Configuration
 
 describe 'Client CLI integration' do
 
   include OpenSSLHelpers
+
+  let :uri do
+    URI(config['client']['server'])
+  end
+
+  def server_up?
+    begin
+      Net::HTTP.start(uri.host, uri.port) do |http|
+        http.get '/'
+      end
+    rescue Errno::ECONNREFUSED
+      return false
+    end
+    true
+  end
 
   before :all do
     FileUtils.rm_f CERTIFICATE_PATH
@@ -16,6 +32,10 @@ describe 'Client CLI integration' do
       thread = Thread.new do
         Signet::HTTPServer.run!
       end
+      Timeout.timeout(60) do
+        thread.join(0.1) until server_up?
+      end
+
       `bin/signet-client`
     ensure
       thread.exit
